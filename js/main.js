f85fc761b48e9a64e6613147c1e32ad6e4fc3965
Joshua Nobleza <joshnobleza@bell.net>
2023-07-17 23:28:46 -0400

    refactor: Refactor production code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 // Show or hide the user dropdown menu when user clicks on the profile image
-document.getElementById('profile-img').addEventListener('click', function(evt) {
+document.getElementById('profile-img').addEventListener('click', function() {
     this.nextElementSibling.classList.toggle('hidden');
 });
